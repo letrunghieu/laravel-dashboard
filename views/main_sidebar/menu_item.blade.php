f7960092bbec79afa690a07a3ad5adebfd118bca
Hieu Le <letrunghieu.cse09@gmail.com>
2015-11-17 16:47:21 +0700

    Wrap the link inside the span element

--- views/main_sidebar/menu_item.blade.php
+++ views/main_sidebar/menu_item.blade.php
@@ -2,7 +2,7 @@
     <li class="menuitem treeview {{ app('menu.manager')->isActive($item) ? 'active' : '' }}">
         <a href="#">
             {!! $item['before'] !!}
-            {{ $item['item']->getLabel()  }}
+            <span>{{ $item['item']->getLabel() }}</span>
             <i class="fa fa-angle-left pull-right"></i>
         </a>
         @include(\HieuLe\LaravelDashboard\Dashboard::PLUGIN_NAME.'::main_sidebar.sub_menu', ['menu' => $item['item']])
@@ -11,7 +11,7 @@
     <li class="menuitem item {{ app('menu.manager')->isActive($item) ? 'active' : '' }}">
         <a href="{{ $item['item']['url']  }}">
             {!! $item['before'] !!}
-            {{ $item['item']['text']  }}
+            <span>{{ $item['item']['text'] }}</span>
             {!! $item['after'] !!}
         </a>
     </li>
